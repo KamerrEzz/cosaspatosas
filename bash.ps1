59879424a9d774d81adb2834f0f686e9a48239bb
Kamerr Ezz <kamerr_oficial@hotmail.com>
2024-04-11 19:07:49 -0700

    Update bash.ps1

--- bash.ps1
+++ bash.ps1
@@ -1,3 +1,4 @@
+# new-item $PROFILE -Force -ItemType file
 # oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/schema.json' | Invoke-Expression 
 oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/huvix.omp.json' | Invoke-Expression
 # aliase para crear un commit para agregar git add . y git commit -am "mensaje"
